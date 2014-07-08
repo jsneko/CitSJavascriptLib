1680d4a9c7075c0c47b0406da6302b277c82c73a
jsneko <william.weithers@gmail.com>
2014-07-07 20:53:19 -0400

    - updated gruntfile.js according to online instructions. not sure if it will work or not. - updated utils.js by adding the parseFunctionParameters() method to the returned object. - updated proxymanager to allow the creation of prototype proxies: so you may get a proxy of an instance object rather than only a singleton.

--- gruntfile.js
+++ gruntfile.js
@@ -2,14 +2,20 @@
  * Created by wweithers on 6/28/2014.
  */
 
-grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-        utils: {
-            'dest/utils.min.js': ['lib/utils.js']
-        },
-        aop: {
-            'dest/aop.min.js': ['lib/utils/aopproxy.js', 'lib/utils/executionevent.js', 'lib/utils/proxymanager.js']
+module.export = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            utils: {
+                'dest/utils.min.js': ['lib/utils.js']
+            },
+            aop: {
+                'dest/aop.min.js': ['lib/utils/aopproxy.js', 'lib/utils/executionevent.js', 'lib/utils/proxymanager.js']
+            }
         }
-    }
-});
+    });
+
+    grunt.loadNpmTask('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['uglify:aop', 'uglify:utils'])
+};
